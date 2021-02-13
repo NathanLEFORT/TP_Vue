991afdc8b14faffa62fd6d4df22faef3c70783c8
klemau <killian.lemau@etudiant.univ-rennes1.fr>
2021-02-13 17:43:00 +0000

    Avancée Recherche.vue

--- src/components/Recherche.vue
+++ src/components/Recherche.vue
@@ -1,14 +1,44 @@
 <template>
   <div>
     <h1>Recherche</h1>
-    <h2>{{utils[0][1]}}</h2>
+    <select v-model="getSpells" id="resultsSize">
+      <option value="50">50</option>
+      <option value="1O0">100</option>
+      <option value="500">500</option>
+      <option value="1000">1000</option>
+    </select>
+    <Table v-bind:sorts="spells"/>
   </div>
 </template>
 
 <script>
+import Table from "@/components/Table";
 export default {
   name: "Recherche",
-  props : ['utils']
+  props : ['utils'],
+  components : {Table},
+  data(){
+    return{
+      spells : [],
+      size : 50,
+      s : []
+    }
+  },
+  mounted() {
+    this.getSpells();
+    this.s.forEach(spell => {
+      if (sessionStorage.getItem(spell[0]) != null) this.spells.push(spell);
+    });
+  },
+  methods: {
+    getSpells(){
+      let newSize = document.querySelector('#resultsSize').value;
+      if(this.size != newSize) {
+        this.size = newSize;
+        this.s = this.$parent.getSpells(this.size);
+      }
+    }
+  }
 }
 </script>
 
