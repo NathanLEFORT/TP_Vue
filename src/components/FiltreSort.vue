614e63376fdb6496f31757d95e3ffdb8534bdc3a
NathanLEFORT <nathan.lefort.d@gmail.com>
2021-02-13 17:40:44 +0100

    debut ligne de filtrage

---
+++ src/components/FiltreSort.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <ligne-filtre v-bind:name="'spellName'"></ligne-filtre>
+    <ligne-filtre v-bind:name="'school'"></ligne-filtre>
+    <ligne-filtre v-bind:name="'branch'"></ligne-filtre>
+    <ligne-filtre v-bind:name="'domain'"></ligne-filtre>
+    <ligne-filtre v-bind:name="'level'"></ligne-filtre>
+    <ligne-filtre v-bind:name="'available'"></ligne-filtre>
+    <ligne-filtre v-bind:name="'add'"></ligne-filtre>
+  </div>
+</template>
+
+<script>
+import LigneFiltre from "@/components/LigneFiltre";
+export default {
+  name: "FiltreSort",
+  components: {LigneFiltre}
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/LigneFiltre.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <a>{{ this.name }}</a>
+    <label>
+      <select>
+        <option></option>
+      </select>
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LigneFiltre",
+  props: ['name']
+}
+</script>
+
+<style scoped>
+
+</style>
