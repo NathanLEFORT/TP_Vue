4a3ddab27787b2ca81341cd3df180df63bbe2e4c
klemau <killian.lemau@etudiant.univ-rennes1.fr>
2021-02-28 18:02:06 +0100

    Fix variés

--- src/components/TableLine.vue
+++ src/components/TableLine.vue
@@ -1,10 +1,10 @@
 <template>
   <tr v-on:click="open()">
     <td v-for="index in 5" :key="index">
-      <div v-if="Array.isArray(sort[index][0])">
+      <div v-if="Array.isArray(sort[index][0])"> <!-- Deals with displaying the fields composed of complex entries, such as Classes and Levels -->
         <p v-for="cont in sort[index]" :key="sort[index].indexOf(cont)">{{cont.join(' ')}}</p>
       </div>
-      <p v-else>{{sort[index].toString()}}</p>
+      <p v-else>{{sort[index].toString()}}</p> <!-- Display simple fields such as the name -->
     </td> <!--  -->
     <td>
       Click to show more details
